a277e4fa1b3e1b7f76e65f84d237bd790b669238
zhengchunqiu <37128157+zhengchunqiu@users.noreply.github.com>
2023-02-16 16:34:29 +0800

    '搭建秒杀系统'

---
+++ seckill-edu/src/main/java/com/zheng/seckilledu/SeckillEduApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.seckilledu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillEduApplication.class, args);
+    }
+
+}
